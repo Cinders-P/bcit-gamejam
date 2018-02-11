3cc8b489525e7b1d49f578fc8b055bf1126af730
Evoiis <athonly@gmail.com>
2018-02-10 16:16:31 -0800

    Added Player Movement

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local sti = require "lib/sti"
 
 local player = require "src/player"
-local map = {}
+map = {}
 
 function love.load(arg)
     map = sti("maps/room1.lua")
@@ -9,10 +9,12 @@ end
 
 function love.update(dt)
     player.addTime(dt)
+    player.move(key,dt)
+    
 end
 
 function love.draw()
     map:draw(0, 0, 2, 2)
-    player.draw(0, 0)
---    love.graphics.print("Hello World", 400, 300)
+    player.draw(player.act_x,player.act_y)
 end
+
